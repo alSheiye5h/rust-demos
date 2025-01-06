ec7f576dda2c43a4abb07a94803524c4b0e72409
alSeiye5h <anasbelkhadir13@gmail.com>
2025-01-06 21:01:41 +0100

    add

--- mini_redis/src/main.rs
+++ mini_redis/src/main.rs
@@ -1,17 +1,30 @@
-use mini_redis::{client, Result};
+// use mini_redis::{client, Result};
+
+// #[tokio::main]
+// async fn main() -> Result<()> {
+//     // connect to mini-redis address
+//     let mut client = client::connect("127.0.0.1:6379").await?;
+
+//     // set the key "hello" with value world
+//     client.set("hello", "world".into()).await?;
+
+//     let result = client.get("hello").await?;
+
+//     println!("got value from server, result={:?}", result.unwrap());
+
+//     Ok(())
+// }
+
+async fn say_world() {
+    println!("world");
+}
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    // connect to mini-redis address
-    let mut client = client::connect("127.0.0.1:6379").await?;
+async fn main() {
+    let oper = say_world();
+    println!("hello");
+    oper.await;
 
-    // set the key "hello" with value world
-    client.set("hello", "world".into()).await?;
-
-    let result = client.get("hello").await?;
-
-    println!("got value from server, result={:?}", result.unwrap());
-
-    Ok(())
 }
+
 
