b909629cb139a157bf29110e241b0cd66a30d1f0
alSeiye5h <anasbelkhadir13@gmail.com>
2025-01-12 22:21:23 +0100

    ps

--- mini_redis/src/example/hello-redis.rs
+++ mini_redis/src/example/hello-redis.rs
@@ -2,13 +2,13 @@ use mini_redis::{client, Result};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // Open a connection to the mini-redis address.
+    // tconnecta b mini redis
     let mut client = client::connect("127.0.0.1:6379").await?;
 
-    // Set the key "hello" with value "world"
+    // set l value world l key hello
     client.set("hello", "world".into()).await?;
 
-    // Get key "hello"
+    // Get lkey hello
     let result = client.get("hello").await?;
 
     println!("got value from the server; result={:?}", result);
--- mini_redis/src/main.rs
+++ mini_redis/src/main.rs
@@ -1,37 +1,47 @@
+use bytes::Bytes;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 use tokio::net::{TcpListener, TcpStream};
 use mini_redis::{Connection, Frame};
 
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
+
 #[tokio::main]
 async fn main() {
-    // Bind the listener to the address
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
+    println!("Listening");
+
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
-        // The second item contains the IP and port of the new connection.
         let (socket, _) = listener.accept().await.unwrap();
-        process(socket).await;
+
+        let db = db .clone();
+
+        println!("Accepted");
+        tokio::spawn(async move {
+            process(socket, db).await
+        });
     }
 }
 
-
-
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream, db: Db) {
     use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
-
-    let mut db = HashMap::new();
 
     let mut connection = Connection::new(socket);
 
     while let Some(frame) = connection.read_frame().await.unwrap() {
-        let response =  match Command::from_frame(frame).unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
-                Frame::Simple("OK".to_string()) 
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
+                Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
-                    Frame::Bulk(value.clone().into())
+                    Frame::Bulk(value.clone())
                 } else {
                     Frame::Null
                 }
@@ -40,6 +50,94 @@ async fn process(socket: TcpStream) {
         };
 
         connection.write_frame(&response).await.unwrap();
-
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// use tokio::net::{TcpListener, TcpStream};
+// use mini_redis::{Connection, Frame};
+
+// #[tokio::main]
+// async fn main() {
+//     // bindi l ip w lport => yaeni launchi server li ghyaccepti lconnections
+//     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+//     loop {
+//         // hna mn listener ghanakhod socket, w dk l element tani fih l ip w lport dial l client
+//         let (socket, _) = listener.accept().await.unwrap();
+//         process(socket).await;
+//     }
+// }
+
+// async fn process(socket: TcpStream) {
+//     use mini_redis::Command::{self, Get, Set};
+//     use std::collections::HashMap;
+
+//     // hna decalarina wahed lhashmap li katstore json data
+//     let mut db = HashMap::new();
+
+//     // hna khdina socket w dkhlnah w pipe wla connection jdida
+//     let mut connection = Connection::new(socket);
+
+//     while let Some(frame) = connection.read_frame().await.unwrap() {
+//         let response =  match Command::from_frame(frame).unwrap() {
+//             Set(cmd) => {
+//                 db.insert(cmd.key().to_string(), cmd.value().to_vec());
+//                 Frame::Simple("OK".to_string()) 
+//             }
+//             Get(cmd) => {
+//                 if let Some(value) = db.get(cmd.key()) {
+//                     Frame::Bulk(value.clone().into())
+//                 } else {
+//                     Frame::Null
+//                 }
+//             }
+//             cmd => panic!("unimplemented {:?}", cmd),
+//         };
+
+//         connection.write_frame(&response).await.unwrap();
+
+//     }
+// }
