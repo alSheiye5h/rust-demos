1194b18a6a813a8f9f2d721f4867d56d78484cf5
alSeiye5h <anasbelkhadir13@gmail.com>
2025-01-12 21:36:03 +0100

    ps

--- mini_redis/src/main.rs
+++ mini_redis/src/main.rs
@@ -1,5 +1,19 @@
-use tokio::TcpStream;
+use tokio::net::{TcpListener, TcpStream};
 use mini_redis::{Connection, Frame};
+
+#[tokio::main]
+async fn main() {
+    // Bind the listener to the address
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    loop {
+        // The second item contains the IP and port of the new connection.
+        let (socket, _) = listener.accept().await.unwrap();
+        process(socket).await;
+    }
+}
+
+
 
 async fn process(socket: TcpStream) {
     use mini_redis::Command::{self, Get, Set};
@@ -10,6 +24,22 @@ async fn process(socket: TcpStream) {
     let mut connection = Connection::new(socket);
 
     while let Some(frame) = connection.read_frame().await.unwrap() {
-        let response = match Command::from
+        let response =  match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string()) 
+            }
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented {:?}", cmd),
+        };
+
+        connection.write_frame(&response).await.unwrap();
+
     }
 }
