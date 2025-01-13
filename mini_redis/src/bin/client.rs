4c38a5fb44bfbd6400f368dc9fd4c2539fec1bed
alSeiye5h <anasbelkhadir13@gmail.com>
2025-01-13 14:49:24 +0100

    ps

--- mini_redis/src/bin/client.rs
+++ mini_redis/src/bin/client.rs
@@ -18,6 +18,27 @@ async fn main() {
     }
 }
 
+use mini_redis::client;
+// The `move` keyword is used to **move** ownership of `rx` into the task.
+let manager = tokio::spawn(async move {
+    // Establish a connection to the server
+    let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+
+    // Start receiving messages
+    while let Some(cmd) = rx.recv().await {
+        use Command::*;
+
+        match cmd {
+            Get { key } => {
+                client.get(&key).await;
+            }
+            Set { key, val } => {
+                client.set(&key, val).await;
+            }
+        }
+    }
+});
+
 
 
 
