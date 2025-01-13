29acd28ae9ca067845ca70fcbbad3d462124808f
alSeiye5h <anasbelkhadir13@gmail.com>
2025-01-13 14:51:37 +0100

    ps

--- mini_redis/src/bin/client.rs
+++ mini_redis/src/bin/client.rs
@@ -37,7 +37,31 @@ let manager = tokio::spawn(async move {
             }
         }
     }
+
+    let tx2 = tx.clone();
+
+// Spawn two tasks, one gets a key, the other sets a key
+let t1 = tokio::spawn(async move {
+    let cmd = Command::Get {
+        key: "foo".to_string(),
+    };
+
+    tx.send(cmd).await.unwrap();
 });
+
+let t2 = tokio::spawn(async move {
+    let cmd = Command::Set {
+        key: "foo".to_string(),
+        val: "bar".into(),
+    };
+
+    tx2.send(cmd).await.unwrap();
+});
+t1.await.unwrap();
+t2.await.unwrap();
+manager.await.unwrap();
+});
+
 
 
 
--- mini_redis/src/bin/server.rs
+++ mini_redis/src/bin/server.rs
@@ -1,60 +1,79 @@
+// use bytes::Bytes;
+// use std::collections::HashMap;
+// use std::sync::{Arc, Mutex};
+// use tokio::net::{TcpListener, TcpStream};
+// use mini_redis::{Connection, Frame};
+
+// type Db = Arc<Mutex<HashMap<String, Bytes>>>;
+
+
+// #[tokio::main]
+// async fn main() {
+//     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+//     println!("Listening");
+
+//     let db = Arc::new(Mutex::new(HashMap::new()));
+
+//     loop {
+//         let (socket, _) = listener.accept().await.unwrap();
+
+//         let db = db .clone();
+
+//         println!("Accepted");
+//         tokio::spawn(async move {
+//             process(socket, db).await
+//         });
+//     }
+// }
+
+// async fn process(socket: TcpStream, db: Db) {
+//     use mini_redis::Command::{self, Get, Set};
+
+//     let mut connection = Connection::new(socket);
+
+//     while let Some(frame) = connection.read_frame().await.unwrap() {
+//         let response = match Command::from_frame(frame).unwrap() {
+//             Set(cmd) => {
+//                 let mut db = db.lock().unwrap();
+//                 db.insert(cmd.key().to_string(), cmd.value().clone());
+//                 Frame::Simple("OK".to_string())
+//             }
+//             Get(cmd) => {
+//                 let db = db.lock().unwrap();
+//                 if let Some(value) = db.get(cmd.key()) {
+//                     Frame::Bulk(value.clone())
+//                 } else {
+//                     Frame::Null
+//                 }
+//             }
+//             cmd => panic!("unimplemented {:?}", cmd),
+//         };
+
+//         connection.write_frame(&response).await.unwrap();
+//     }
+// }
+
+use tokio::sync::oneshot;
 use bytes::Bytes;
-use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
-use tokio::net::{TcpListener, TcpStream};
-use mini_redis::{Connection, Frame};
 
-type Db = Arc<Mutex<HashMap<String, Bytes>>>;
-
-
-#[tokio::main]
-async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
-
-    println!("Listening");
-
-    let db = Arc::new(Mutex::new(HashMap::new()));
-
-    loop {
-        let (socket, _) = listener.accept().await.unwrap();
-
-        let db = db .clone();
-
-        println!("Accepted");
-        tokio::spawn(async move {
-            process(socket, db).await
-        });
-    }
+/// Multiple different commands are multiplexed over a single channel.
+#[derive(Debug)]
+enum Command {
+    Get {
+        key: String,
+        resp: Responder<Option<Bytes>>,
+    },
+    Set {
+        key: String,
+        val: Bytes,
+        resp: Responder<()>,
+    },
 }
 
-async fn process(socket: TcpStream, db: Db) {
-    use mini_redis::Command::{self, Get, Set};
-
-    let mut connection = Connection::new(socket);
-
-    while let Some(frame) = connection.read_frame().await.unwrap() {
-        let response = match Command::from_frame(frame).unwrap() {
-            Set(cmd) => {
-                let mut db = db.lock().unwrap();
-                db.insert(cmd.key().to_string(), cmd.value().clone());
-                Frame::Simple("OK".to_string())
-            }
-            Get(cmd) => {
-                let db = db.lock().unwrap();
-                if let Some(value) = db.get(cmd.key()) {
-                    Frame::Bulk(value.clone())
-                } else {
-                    Frame::Null
-                }
-            }
-            cmd => panic!("unimplemented {:?}", cmd),
-        };
-
-        connection.write_frame(&response).await.unwrap();
-    }
-}
-
-
+/// Provided by the requester and used by the manager task to send
+/// the command response back to the requester.
+type Responder<T> = oneshot::Sender<mini_redis::Result<T>>;
 
 // type SharedDb = Arc<Vec<Mutex<HashMap<String, Vec<u8>>>>>;
 
