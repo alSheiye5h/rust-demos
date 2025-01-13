ea2ecb482c2382fa0dee1a1075f2ab859e519a46
alSeiye5h <anasbelkhadir13@gmail.com>
2025-01-13 15:17:24 +0100

    ps

--- mini_redis/src/bin/server.rs
+++ mini_redis/src/bin/server.rs
@@ -53,38 +53,55 @@
 //         connection.write_frame(&response).await.unwrap();
 //     }
 // }
+let t1 = tokio::spawn(async move {
+    let (resp_tx, resp_rx) = oneshot::channel();
+    let cmd = Command::Get {
+        key: "foo".to_string(),
+        resp: resp_tx,
+    };
 
-use tokio::sync::oneshot;
-use bytes::Bytes;
+    // Send the GET request
+    tx.send(cmd).await.unwrap();
 
-/// Multiple different commands are multiplexed over a single channel.
-#[derive(Debug)]
-enum Command {
-    Get {
-        key: String,
-        resp: Responder<Option<Bytes>>,
-    },
-    Set {
-        key: String,
-        val: Bytes,
-        resp: Responder<()>,
-    },
-}
+    // Await the response
+    let res = resp_rx.await;
+    println!("GOT = {:?}", res);
+});
+
+let t2 = tokio::spawn(async move {
+    let (resp_tx, resp_rx) = oneshot::channel();
+    let cmd = Command::Set {
+        key: "foo".to_string(),
+        val: "bar".into(),
+        resp: resp_tx,
+    };
+
+    // Send the SET request
+    tx2.send(cmd).await.unwrap();
+
+    // Await the response
+    let res = resp_rx.await;
+    println!("GOT = {:?}", res);
+});
 
 /// Provided by the requester and used by the manager task to send
 /// the command response back to the requester.
 type Responder<T> = oneshot::Sender<mini_redis::Result<T>>;
 
-// type SharedDb = Arc<Vec<Mutex<HashMap<String, Vec<u8>>>>>;
-
-// fn new_shared_db(num_shards: usize) -> SharedDb {
-//     let mut db = Vec::with_capacity(num_shards);
-//     for _ in 0..num_shards {
-//         db.push(Mutex::new(HashMap::new()));
-//     }
-//     Arc::new(db)
-// }
-
+while let Some(cmd) = rx.recv().await {
+    match cmd {
+        Command::Get { key, resp } => {
+            let res = client.get(&key).await;
+            // Ignore errors
+            let _ = resp.send(res);
+        }
+        Command::Set { key, val, resp } => {
+            let res = client.set(&key, val).await;
+            // Ignore errors
+            let _ = resp.send(res);
+        }
+    }
+}
 
 
 
